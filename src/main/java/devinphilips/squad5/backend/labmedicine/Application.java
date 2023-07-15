1bc64d91b206f8c428d5b634358582f0e3e1db61
jtspinelli <contato@jonathanspinelli.com>
2023-07-14 23:50:14 -0300

    initial setup

---
+++ src/main/java/devinphilips/squad5/backend/labmedicine/Application.java
@@ -0,0 +1,13 @@
+package devinphilips.squad5.backend.labmedicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
