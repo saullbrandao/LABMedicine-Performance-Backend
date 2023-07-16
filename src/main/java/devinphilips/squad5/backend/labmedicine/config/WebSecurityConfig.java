bc9bb3f1085008d40899be2a4ee89d8d4101bd07
jtspinelli <contato@jonathanspinelli.com>
2023-07-16 00:23:37 -0300

    feat: add 'WebSecurityConfig' file

---
+++ src/main/java/devinphilips/squad5/backend/labmedicine/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package devinphilips.squad5.backend.labmedicine.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
