ba3726fe53818121356f077fe8ce8691b090c9ad
Saull Brandão <saull@outlook.com>
2023-07-19 09:11:32 -0300

    feat: add method to get patient by id

---
+++ src/main/java/devinphilips/squad5/backend/labmedicine/services/PatientService.java
@@ -0,0 +1,19 @@
+package devinphilips.squad5.backend.labmedicine.services;
+
+import devinphilips.squad5.backend.labmedicine.models.Patient;
+import devinphilips.squad5.backend.labmedicine.repositories.PatientRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PatientService {
+    private final PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public Patient getById(Integer id) {
+        return patientRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Paciente não encontrado"));
+    }
+}
