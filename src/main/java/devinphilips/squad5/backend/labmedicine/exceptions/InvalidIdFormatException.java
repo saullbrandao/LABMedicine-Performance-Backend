c8faf6e0297c038b2fd53a1dd7ef5082b0423f7b
jtspinelli <contato@jonathanspinelli.com>
2023-07-21 11:55:28 -0300

    refactor: code clean-up

--- src/main/java/devinphilips/squad5/backend/labmedicine/exceptions/InvalidIdFormatException.java
+++
@@ -1,7 +0,0 @@
-package devinphilips.squad5.backend.labmedicine.exceptions;
-
-public class InvalidIdFormatException extends Exception {
-    public InvalidIdFormatException(){
-        super("Invalid id format");
-    }
-}
