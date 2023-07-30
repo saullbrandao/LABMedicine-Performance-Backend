98f98648a430f27fcdfb6e0c845b60b7c621d5cf
jtspinelli <contato@jonathanspinelli.com>
2023-07-30 18:16:48 -0300

    refactor: add generic methods for logging creating, updating and deleting operations to LogService

--- src/main/java/devinphilips/squad5/backend/labmedicine/services/LogService.java
+++ src/main/java/devinphilips/squad5/backend/labmedicine/services/LogService.java
@@ -1,6 +1,8 @@
 package devinphilips.squad5.backend.labmedicine.services;
 
+import devinphilips.squad5.backend.labmedicine.dtos.user.UserResponseDTO;
 import devinphilips.squad5.backend.labmedicine.models.Log;
+import devinphilips.squad5.backend.labmedicine.models.Patient;
 import devinphilips.squad5.backend.labmedicine.repositories.LogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,5 +20,29 @@ public class LogService {
 
     public Log save(Log log) {
         return logRepository.save(log);
+    }
+
+    public void registerCreate(UserResponseDTO user, Patient patient, String entity) {
+        var log = new Log(
+                String.format("O %s %s cadastrou %s para o(a) paciente %s (%s)", user.type(), user.name(), entity, patient.getName(), patient.getCpf())
+        );
+
+        save(log);
+    }
+
+    public void registerUpdate(UserResponseDTO user, Patient patient, Integer entityId, String entity) {
+        var log = new Log(
+                String.format("O %s %s atualizou %s (id: %s) do(a) paciente %s (%s)", user.type(), user.name(), entity, entityId, patient.getName(), patient.getCpf())
+        );
+
+        save(log);
+    }
+
+    public void registerDelete(UserResponseDTO user, Patient patient, Integer entityId, String entity) {
+        var log = new Log(
+                String.format("O %s %s excluiu %s (id: %s) do(a) paciente %s (%s)", user.type(), user.name(), entity, entityId, patient.getName(), patient.getCpf())
+        );
+
+        save(log);
     }
 }
